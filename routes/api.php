019bf409fe3f67b3c634d4c644f4d816340713ad
KPrincessCode <kyleprincesssitjar982@gmail.com>
2026-04-25 22:47:14 -0700

    Add admin notes column to bookings

--- routes/api.php
+++ routes/api.php
@@ -10,8 +10,11 @@ Route::get('/pets/{id}', [PetController::class, 'show']);
 Route::put('/pets/{id}', [PetController::class, 'update']);
 Route::delete('/pets/{id}', [PetController::class, 'destroy']);
 
+
 Route::get('/bookings', [BookingController::class, 'index']);
 Route::post('/bookings', [BookingController::class, 'store']);
 Route::get('/bookings/{id}', [BookingController::class, 'show']);
 Route::put('/bookings/{id}', [BookingController::class, 'update']);
 Route::delete('/bookings/{id}', [BookingController::class, 'destroy']);
+Route::put('/bookings/{id}/approve', [BookingController::class, 'approve']);
+Route::put('/bookings/{id}/reject', [BookingController::class, 'reject']);
